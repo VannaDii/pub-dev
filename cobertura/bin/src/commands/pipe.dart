5390dcfdd21ce19029db5b2c6de7ad9175d9655b
Gio Palacino <gio@palacino.net>
2022-01-15 09:50:39 -0600

    feat(cobertura) adds placeholder pipe command

---
+++ cobertura/bin/src/commands/pipe.dart
@@ -0,0 +1,10 @@
+/**
+ * Add Pipe Support
+ *
+ * Pipe reads from STDIN expecting LCOV
+ * Pipe writes to STDOUT producing Cobertura
+ *
+ * Optionally support a --gcov flag to indicate the input is GCOV
+ *
+ * Explore piping input directly from `dart test`
+ */
