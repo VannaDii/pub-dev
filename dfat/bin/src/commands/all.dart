c6d235444042a9795fa68183fd60904878f2957c
Gio Palacino <gio@palacino.net>
2022-01-30 15:29:34 -0600

    chore(dfat): removes dfat

--- dfat/bin/src/commands/all.dart
+++
@@ -1,9 +0,0 @@
-export 'aggregate.dart';
-export 'build.dart';
-export 'check.dart';
-export 'docker.dart';
-export 'install.dart';
-export 'lambda.dart';
-export 'test.dart';
-export 'shared.dart';
-export 'validate.dart';
--- dfat/bin/src/enums.dart
+++
@@ -1,3 +0,0 @@
-enum KnownPaths { dfat, iac, lambdas, schemas, shared, assets }
-
-enum CpuArch { arm64v8, amd64, windowsAmd64 }
